461ed5aa3d3f3f83d226ca9d028431276e15eb11
M Aswin Kishore <mak26.work@gmail.com>
2021-08-22 16:37:47 +0530

    Create send_mail.js

---
+++ send_mail.js
@@ -0,0 +1,50 @@
+require('colors');
+const csv = require('csv-parser');
+const fs = require('fs');
+// eslint-disable-next-line no-unused-vars
+const { devMail, backendMail } = require('./util/constants');
+const { sendNoReplyMail, MailtokenVerifed } = require('./util/mailHandler');
+
+/** @typedef {import('./models/model').Event} Event */
+/** @typedef {import('./models/model').Person} Person */
+
+async function csvParse() {
+    const html = fs.readFileSync('./temp/content-accept.html', 'utf-8', () => {});
+
+    const attachment = [
+        {
+            filename: 'dsc.png',
+            path: './temp/logo.png',
+            cid: 'logo',
+        },
+        {
+            filename: 'dsc-1.png',
+            path: './temp/gdsc.png',
+            cid: 'gdsc',
+        },
+    ];
+
+    let id = 1;
+    const toLeft = [];
+    // toLeft.push(sendNoReplyMail(devMail, 'R’21 | Final Phase Results', html.replace('<#NAME>', 'Backend'), attachment, id++));
+    await new Promise((resolve) =>
+        fs
+            .createReadStream('./temp/batch_acceptance.csv')
+            .pipe(csv())
+            .on('data', (e) => {
+                // toLeft.push(sendNoReplyMail(e.MAIL, 'R’21 | Final Phase Results', html.replace('<#NAME>', e.NAME), attachment, id++));
+            })
+            .on('end', () => {
+                resolve();
+            })
+    );
+
+    await Promise.all(toLeft);
+}
+
+(async () => {
+    await MailtokenVerifed;
+    await csvParse();
+    console.log('\nEmail Sending Done'.magenta.bold);
+    process.exit(0);
+})();
