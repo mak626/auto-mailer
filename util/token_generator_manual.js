bd031fedd03cef4d029b13ebe0bd9ad89988fa80
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-05-07 18:13:38 +0530

    feat: token-generator is automated

--- util/token_generator_manual.js
+++ util/token_generator_manual.js
@@ -1,42 +1,26 @@
 /* eslint-disable camelcase */
-const readline = require('readline');
+const http = require('http');
+require('colors');
 const fs = require('fs');
 const { google } = require('googleapis');
 
 const SCOPES = ['https://mail.google.com/'];
-
 const TOKEN_PATH = './assets/tokens/mail.json';
+const CLIENT_PATH = './assets/tokens/client.json';
+
+let oAuth2Client;
 
 /**
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token
  */
-function getNewToken(oAuth2Client, callback) {
+function getNewToken() {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
     });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (e) => {
-                if (e) return console.error(e);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+    console.log('Authorize email by visiting this url:'.blue.bold, authUrl);
 }
 
 /**
@@ -47,7 +31,7 @@ function getNewToken(oAuth2Client, callback) {
  */
 function authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.web;
-    const oAuth2Client = new google.auth.OAuth2(
+    oAuth2Client = new google.auth.OAuth2(
         client_id,
         client_secret,
         redirect_uris[1] // localhost
@@ -55,13 +39,41 @@ function authorize(credentials, callback) {
 
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
+        if (err) return getNewToken();
         oAuth2Client.setCredentials(JSON.parse(token));
         callback(oAuth2Client);
     });
 }
 
-fs.readFile('./assets/tokens/client.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    authorize(JSON.parse(content), (authCode) => console.log(authCode));
+const app = http.createServer((req, res) => {
+    if (req.url.includes('code')) {
+        const url = new URL(`${req.protocol}://${req.headers.host}${req.url}`);
+        const code = url.searchParams.get('code');
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                const errorMessage = 'Error retrieving access token';
+                console.error(errorMessage.red.bold, err);
+                res.writeHead(401, 'Failed');
+                res.end(`<h1>${errorMessage}</h1><p>${err.message}</p>`);
+                return;
+            }
+            oAuth2Client.setCredentials(token);
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token, null, 4), (e) => {
+                if (e) return console.error(e);
+                console.log('Token stored to'.green.bold, TOKEN_PATH);
+                res.writeHead(200, 'Success');
+                res.end(`<h1>Success</h1><p>Token stored to${TOKEN_PATH}</p>`);
+            });
+        });
+    }
 });
+
+app.on('listening', () => {
+    fs.readFile(CLIENT_PATH, (err, content) => {
+        if (err) return console.log('Error loading client secret file:', err);
+        authorize(JSON.parse(content), (authCode) => console.log(authCode));
+    });
+});
+
+app.listen(3000);
